7e2eb2b95598c26df78f1a6bc41a58b753059199
gdzg <1602270891@qq.com>
2023-12-11 04:53:41 +0800

    Initial commit

--- Assets/com.overmine.editor/Editor/Locator/Dummy.cs
+++ Assets/com.overmine.editor/Editor/Locator/Dummy.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 namespace Editor.Locator
 {
     // Dummy scriptable object to locate this folder.
-    // TODO: Maybe find a better way to do this.
     public class Dummy : ScriptableObject
     {
         
