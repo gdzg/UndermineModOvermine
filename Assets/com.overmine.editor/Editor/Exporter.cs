32b3d7be3428cbb257cfd802c04277fa67686b04
gdzg <153278007+gdzg@users.noreply.github.com>
2024-10-18 17:19:49 +0800

    Update Exporter.cs

--- Assets/com.overmine.editor/Editor/Exporter.cs
+++ Assets/com.overmine.editor/Editor/Exporter.cs
@@ -122,7 +122,7 @@ namespace Editor
                     Registry.LocalMachine.OpenSubKey(
                         @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 656350"))
                 {
-                    return (key?.GetValue("InstallLocation")?.ToString())??@"F:\game\UnderMine.v1.2.1.Multi.9";
+                    return key?.GetValue("InstallLocation")?.ToString();
                 }
             }
             catch (Exception)
